4b4e21c3546c81c961193755840ff0490cc24c69
Kelvinhcs <kelvinhcosta@hotmail.com>
2025-05-29 19:05:42 -0300

    changing the way i identify the commands

--- main.py
+++ main.py
@@ -1,55 +1,49 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
 import discord
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
+#loading token from .env
 load_dotenv()
 token = os.getenv('DISCORD_BOT_TOKEN')
 
+#setting the default permissions and the prefix
 intents = discord.Intents.default()
 intents.message_content = True
-bot = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-g1 = requests.get(url="https://g1.globo.com")
-soup = BeautifulSoup(g1.content, 'html5lib')
-
-
+#url and soup section
+g1Url = requests.get(url="https://g1.globo.com")
+soupG1 = BeautifulSoup(g1Url.content, 'html5lib')
 
 
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
+    print(f'Iniciado e logado como: {bot.user}')
 
-@bot.event
-async def on_message(message):
-    lowerMessage = message.content.lower()
-    if message.author == bot.user:
-        return
 
-    elif lowerMessage.startswith('$on'):
-        await message.channel.send('Olá, o bot está funcionando perfeitamente até o momento')
+@bot.command()
+async def news(ctx):
+    await ctx.reply('Carregando notícias...')
+    newsMessage = '📰-=-=-=-=-=-=-=-=-=-=- **Notícias do G1** -=-=-=-=-=-=-=-=-=-=-📰\n\n'
+    noticias = soupG1.find_all(attrs={"elementtiming": "text-ssr"})
 
-    elif lowerMessage.startswith('$ping'):
-        await message.channel.send(f'Pong! {bot.latency * 1000:.2f}ms')
+    for contador, item in enumerate(noticias, start=1):
+        link = item.find_parent('a')
+        if link and link.get('href'):
+            newsMessage += (
+                f'{contador} - **{item.get_text(strip=True)}**\n'
+                f'Link: <{link["href"]}>\n\n'
+            )
+    if len(newsMessage) <= 200:
+        await ctx.send(newsMessage)
+    else:
+        parts = [newsMessage[i:i + 2000] for i in range(0, len(newsMessage), 2000)]
+        for part in parts:
+            await ctx.send(part)
 
-    elif lowerMessage.startswith('$g1'):
-        await message.channel.send('Carregando notícias...')
-        newsMessage = '📰-=-=-=-=-=-=-=-=-=-=- **News G1** -=-=-=-=-=-=-=-=-=-=-📰\n\n'
-        noticias = soup.find_all(attrs={"elementtiming": "text-ssr"})
 
-        for contador, item in enumerate(noticias, start=1):
-            link = item.find_parent('a')
-            if link and link.get('href'):
-                newsMessage += (
-                    f'{contador} - **{item.get_text(strip=True)}**\n'
-                    f'Link: <{link["href"]}>\n\n'
-                )
-        if len(newsMessage) <= 200:
-            await message.channel.send(newsMessage)
-        else:
-            parts = [newsMessage[i:i+2000] for i in range(0, len(newsMessage), 2000)]
-            for part in parts:
-                await message.channel.send(part)
 
 bot.run(token)
