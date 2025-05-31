7120fd74bf31001ec6164c021e289d19d18eeaeb
Kelvinhcs <kelvinhcosta@hotmail.com>
2025-05-30 22:30:30 -0300

    Refactoring the news command and adding embed messages to the bot

--- main.py
+++ main.py
@@ -14,11 +14,6 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='$', intents=intents)
 
-#url and soup section
-g1Url = requests.get(url="https://g1.globo.com")
-soupG1 = BeautifulSoup(g1Url.content, 'html5lib')
-valorUrl = requests.get(url="https://valor.globo.com/")
-soupValor = BeautifulSoup(valorUrl.content, 'html5lib')
 
 
 @bot.event
@@ -28,6 +23,14 @@ async def on_ready():
 
 @bot.command()
 async def news(ctx):
+
+    #url and soup section
+    g1Url = requests.get(url="https://g1.globo.com")
+    soupG1 = BeautifulSoup(g1Url.content, 'html5lib')
+    valorUrl = requests.get(url="https://valor.globo.com/")
+    soupValor = BeautifulSoup(valorUrl.content, 'html5lib')
+
+
     await ctx.reply('Carregando notícias...')
     newsMessage = '📰-=-=-=-=-=-=-=-=-=-=- **Notícias do G1** -=-=-=-=-=-=-=-=-=-=-📰\n\n'
     noticias = soupG1.find_all(attrs={"elementtiming": "text-ssr"})
@@ -46,27 +49,29 @@ async def news(ctx):
         for part in parts:
             await ctx.send(part)
 
-    newsMessage = '📰-=-=-=-=-=-=-=-=-=-=- **Notícias do Valor** -=-=-=-=-=-=-=-=-=-=-📰\n\n'
+
+@bot.command()
+async def valor(ctx):
+    color_hex = "#016667"
+    discord_color = int(color_hex[1:], 16)
+    embed = discord.Embed(title="Valor Econômico", color=discord_color, url='https://valor.globo.com/')
+    embed.set_author(name="Repositório oficial no github", url='https://github.com/Kelvinhcs/BotNews')
+    embed.set_thumbnail(url="https://s3.glbimg.com/v1/AUTH_1b264e8ce06649ae85acee5d38e32f34/images/novo_logo_valor_economico.png")
+
+    valorUrl = requests.get(url="https://valor.globo.com/")
+    soupValor = BeautifulSoup(valorUrl.content, 'html5lib')
+
     allBlocks = soupValor.find_all('div', class_="highlight__content")
 
     for index, block in enumerate(allBlocks, 1):
-        if index >= 8:
+        if index > 6:
             break
         else:
             title = block.find('h2', class_='highlight__title')
             aTag = block.find('a')
             link = aTag['href']
-            newsMessage += (
-                f'{index} - **{title.get_text(strip=True)}**\n'
-                f'Link: <{link}>\n\n'
-            )
-    if len(newsMessage) <= 200:
-        await ctx.send(newsMessage)
-    else:
-        parts = [newsMessage[i:i + 2000] for i in range(0, len(newsMessage), 2000)]
-        for part in parts:
-            await ctx.send(part)
+            embed.add_field(name=f"{index} - **{title.get_text(strip=True)}**", value=f"{link}", inline=True)
 
-
+    await ctx.send(embed=embed)
 
 bot.run(token)
